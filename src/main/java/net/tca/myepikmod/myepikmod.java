ad4d176b35db33afe14bfd553628b865d0b70a0c
Trey Allen <treytreyallen@gmail.com>
2024-02-10 18:17:43 -0500

    Initial commit!

--- src/main/java/net/tca/myepikmod/myepikmod.java
+++ src/main/java/net/tca/myepikmod/myepikmod.java
@@ -13,6 +13,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
+
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(myepikmod.modId)
 public class myepikmod {
