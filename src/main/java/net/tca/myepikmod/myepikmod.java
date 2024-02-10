9d974a405d1949c58f52e8fc6895ac1ceef25546
Trey Allen <treytreyallen@gmail.com>
2024-02-10 18:17:01 -0500

    Initial commit!

--- src/main/java/net/tca/myepikmod/myepikmod.java
+++ src/main/java/net/tca/myepikmod/myepikmod.java
@@ -14,7 +14,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("myfirstmod")
+@Mod(myepikmod.modId)
 public class myepikmod {
     public static final String modId = "myfirstmod";
     public static final Logger LOGGER = LogUtils.getLogger();
